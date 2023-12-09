e398ff5d2041ba5ccef9e3bb07adbab69c029dc9
Yuri <namyuri@gmail.com>
2023-12-09 14:39:44 +0900

    10.1 AsyncChromiumLoader

--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 # 10. Site GPT
 ## 10.0 Introduction
 ## 10.1 AsyncChromiumLoader
+playwright를 사용해서 (Selenium 비슷한 브라우저 실행하는 것) langchain 주소에서 text data를 추출하는 과정
 ## 10.2 SitemapLoader
 ## 10.3 Parsing Function
 ## 10.4 Map Re Rank Chain
--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,34 @@
 import streamlit as st
 
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+html2text_transformer = Html2TextTransformer()
+
 st.title("SiteGPT")
+
+st.markdown(
+    """
+    # SiteGPT
+            
+    Ask questions about the content of a website.
+            
+    Start by writing the URL of the website on the sidebar.
+"""
+)
+
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+if url:
+    loader = AsyncChromiumLoader([url])
+    docs = loader.load()
+    transformed = html2text_transformer.transform_documents(docs)
